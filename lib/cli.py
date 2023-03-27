c85e3a2144bb8191536bf495f5a70614eefeb21e
Scott <scotthenry1@gmail.com>
2023-03-27 11:32:37 -0400

    It's alive

---
+++ lib/cli.py
@@ -0,0 +1,11 @@
+# from helpers import (
+#     function_1, function_2,
+#     function_3, function_4,
+#     function_5, function_6,
+#     function_7, function_8,
+#     function_9, function_10
+# )
+
+if __name__ == '__main__':
+    print('Welcome to my CLI!')
+    print('Thanks for using my CLI')
