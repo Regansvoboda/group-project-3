63f432ef79d0f34802ac91e5686aec7b68b7775c
Regan Svoboda <regansvoboda@gmail.com>
2023-03-28 09:47:15 -0500

    debug

--- lib/debug.py
+++ lib/debug.py
@@ -1,11 +1,18 @@
-import ipdb;
+#!/usr/bin/env python3
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from db.models import Patient, Doctor, Unit, Visit
 
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///db/hospital.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    patients = [patient for patient in session.query(Patient)]
+    doctors = [doctor for doctor in session.query(Doctor)]
+    units = [unit for unit in session.query(Unit)]
+    visits = [visit for visit in session.query(Visit)]
+    breakpoint()
 
-
-
-
-
-
-ipdb.set_trace()
+    import ipdb; ipdb.set_trace
